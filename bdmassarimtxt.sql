b5129f4658aea959d2b91f370a1463963781af47
luizwfelipe <luizwfelipef@gmail.com>
2024-06-30 22:09:06 -0300

    bd atualizado

--- bdmassarimtxt.sql
+++ bdmassarimtxt.sql
@@ -80,10 +80,18 @@ VALUES ('Luiz', '123', 'luiz@gmail.com', '123.456.789-00', '(11)98765-4321', '20
 
 INSERT INTO categorias (nome) VALUES
 ('Vestidos'),
-('Saias'),
-('Calças'),
-('Sapatos'),
-('Camisas');
+('Calçados'),
+('Jaquetas'),
+('Calças');
 
 INSERT INTO produto (nome, fkIdCategoria,descricao,preco ,estoque, tamanho ,imagem) VALUES
-('SALTO PRETO', 4,'salto lindo, disponível na cor preta, clássico, chique e vesátil.',199.99,9,'38','assets/sapato.jpg');
+('SALTO PRETO', 2,'salto lindo, disponível na cor preta, clássico, chique e vesátil.',199.99,9,'36','assets/sapato.jpg'),
+('JAQUETA VELUDO', 3,'jaqueta atemporal, peluda na cor verde militar. Perfeita para ocasiões de frio, pode ser usada de dois lados.',239.99,9,'M','assets/jaqueta1.png'),
+('VESTIDO PRETO', 1,'um vestido muito bonito, não sei nem as palavras para descreve-lo.',159.99,9,'P','assets/vestidopreto1.png'),
+('PANTALONA ROXA', 4,'calça pantalona, versátil, em uma das cores mais lindas que existem: o roxo, exala riqueza e beleza não se falta.',99.99,9,'G','assets/pantalona1.png'),
+('SAPATILHA COLORIDA', 2,'uma sapatilha com aspecto retrô, chama atenção por causa de sua grande beleza e versatilidade.',129.99,9,'38','assets/sapato1.png'),
+('PANTALONA AZUL', 4,'calça muito linda!',159.99,9,'GG','assets/pantalona2.png'),
+('JAQUETA COURO', 3,'uma jaqueta bem estilosa, perfeito para eventos mais sociais.',269.99,9,'M','assets/jaqueta2.png'),
+('VESTIDO BEGE', 1,'um vestido com caimento diferenciado, apenas para quem entende de moda!',139.99,9,'M','assets/vestidobg.png');
+
+
